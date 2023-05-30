d11edee5de90630dabd4912da58343c29d37317e
Pete West <10847430+peterwest-1@users.noreply.github.com>
2023-05-30 17:36:10 +0200

    Updated build script

--- docker-build.sh
+++ docker-build.sh
@@ -12,7 +12,7 @@ if [[ -z $(git status --porcelain) ]]; then
     echo "New version: $newVersion"
 
     # You can use the $newVersion variable in your script as needed
-    docker build . -t peterwest86/jobjack-client:rest-$newVersion
+    docker build . -t peterwest86/jobjack-client:$newVersion
 else
    echo "Working directory not clean"
 fi
