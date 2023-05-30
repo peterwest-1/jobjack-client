f23cea61c2b57cc27e27ed08d1169ca8827ff872
Pete West <10847430+peterwest-1@users.noreply.github.com>
2023-05-30 15:30:33 +0200

    Fixed error caused from merge confilt resolution

--- src/app/directory.service.ts
+++ src/app/directory.service.ts
@@ -1,11 +1,9 @@
-
+import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DirectoryService {
-
   constructor(private apollo: Apollo) {}
-
 }
